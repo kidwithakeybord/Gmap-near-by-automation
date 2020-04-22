de33f45b77b5f6e720c19342954f8978fa6025c1
Justin Robin M <50717214+kidwithakeybord@users.noreply.github.com>
2020-04-22 20:20:50 +0530

    Update chrome_automation.py

--- chrome_automation.py
+++ chrome_automation.py
@@ -2,14 +2,15 @@
 """
 Spyder Editor
 
-This is a temporary script file.
 """
 from selenium import webdriver
 #setting path for the chromedriver 
 driver = webdriver.Chrome(executable_path='C:/Webderivers/chromedriver.exe')
-
+driver.implicitly_wait(10)
+driver.maximize_window()
 #comadinging it to open kaggle
 driver.get("https://www.kaggle.com");
+driver.implicitly_wait(10)
 #opening a new tab 
 driver.execute_script("window.open('https://github.com','');")
  
